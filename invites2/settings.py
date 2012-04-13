23c980ae10698d7848e9abdfa5db4087141f28be
Chris Amico <eyeseast@gmail.com>
2012-04-13 14:48:04 -0400

    Decluttering

--- invites2/settings.py
+++ invites2/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'invites2db',                      # Or path to database file if using sqlite3.
+        'NAME': 'invites2.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
