98007d009749c1723376b7445a3bd4564328b416
Matt Thompson <service@emailmatt.com>
2012-04-13 14:44:34 -0400

    Fixed settings.py to be portable

--- invites2/settings.py
+++ invites2/settings.py
@@ -1,5 +1,9 @@
 # Django settings for invites project.
 
+import os
+DJANGO_ROOT = os.path.realpath(os.path.dirname(__file__))
+f = lambda fn: os.path.join(DJANGO_ROOT, fn)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -57,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = f('static/assets')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,7 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/mthompson/Documents/Django/invites2/invites2/static',
+    f('static/base'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/mthompson/Documents/Django/invites2/invites2/templates'
+    f('templates'),
 )
 
 INSTALLED_APPS = (
