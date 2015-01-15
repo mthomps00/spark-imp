94ac1417c6824a253fa4f63b387106ee3668d5d8
Matt <matt@emailmatt.com>
2015-01-15 15:00:00 +0000

    Alternate balloting system

--- nod/admin.py
+++ nod/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from nod.models import *
 
+class VoteInline(admin.TabularInline):
+    model = Vote
+    
+class BallotAdmin(admin.ModelAdmin):
+    model = Ballot
+  
+    inlines = [
+      VoteInline,
+    ]
+
 # Register your models here.
 
 admin.site.register(Nomination)
 admin.site.register(Tag)
 admin.site.register(VotingRound)
-admin.site.register(Ballot)
+admin.site.register(Ballot, BallotAdmin)
 admin.site.register(Vote)
