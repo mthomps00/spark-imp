77a32b4675fda59db96318166ae46600808da93e
Matt <matt@emailmatt.com>
2015-01-29 02:37:40 +0000

    Fixed Mailchimp sync issue

--- rsvp/views.py
+++ rsvp/views.py
@@ -268,7 +268,7 @@ def mailsync(request, camptheme):
                 identifier = {
                   'leid': invite.user.sparkprofile.mailchimp_id,
                   }
-                m.lists.update_member(id=id,email={'leid':identifier},merge_vars={'new-email':invite.user.email})
+                m.lists.update_member(id=id,email=identifier,merge_vars={'new-email':invite.user.email})
             else:
                 identifier = {
                   'email': invite.user.email,
